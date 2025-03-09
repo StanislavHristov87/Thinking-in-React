2396a2537dd30a3cdfbc15a99e43225b10f18318
Stanislav Hristov <stanislavhristov0331@gmail.com>
2025-03-09 17:39:30 +0200

    Adding State and props plus functions

---
+++ my-app/src/components/FilterableProductTable/FilterableProductTable.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import ProductTable from "../ProductTable/ProductTable"
+import SearchBar from "../SearchBar/SearchBar";
+
+const FilterableProductTable = ({products}) => {
+
+        const [filterText, setFilterText] = useState('');
+        const [inStockOnly, setInStockOnly] = useState(false);
+        
+        
+
+    return (
+        <div>
+      <SearchBar 
+        filterText={filterText} 
+        inStockOnly={inStockOnly} 
+        onFilterTextChange={setFilterText} 
+        onInStockOnlyChange={setInStockOnly} />
+      <ProductTable 
+        products={products} 
+        filterText={filterText}
+        inStockOnly={inStockOnly} />
+    </div>
+    )
+}
+
+export default FilterableProductTable;
